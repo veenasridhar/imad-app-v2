8636a2502a965497e443fec67228db491fbbdf2e
veenasridhar <veenasridhar96@gmail.com>
2017-02-26 16:10:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get("/article_one",function(req,res)
+{
+    res.send("article-one is requested and will be served here");
+});
+app.get("/article_two",function(req,res)
+{
+    res.send("article-two is requested and will be served here");
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));

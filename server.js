22843429c20c22957c670df5ff131f92b6768ce1
veenasridhar <veenasridhar96@gmail.com>
2017-02-26 16:35:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 app.get("/article_one",function(req,res)
 {
-    res.sendFile(path.join(_dirname,'ui','article_one.html'));
+    res.sendFile(path.join(__dirname,'ui','article_one.html'));
 });
 app.get("/article_two",function(req,res)
 {
